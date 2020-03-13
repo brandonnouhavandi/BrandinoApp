c7eac34d3473fd9cb59dfb7f9db846d588d4b037
Brandon Nouhavandi <brandonnouhavandi@gmail.com>
2020-03-13 10:15:49 -0700

    implemented image defaults for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -8,5 +8,11 @@ end
 
 scope :ruby_on_rails_portfolio_items, -> {where(subtitle: 'Angualar')
 }
+
+after_initialize :set_defaults
+
+def set_defaults
+	self.main_image ||="http://via.placeholder/600x400"
+	self.thumb_image ||="http://via.placeholder/350x200"
+	end
 end
- 
