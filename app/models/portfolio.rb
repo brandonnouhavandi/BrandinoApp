ee5475835aea78846bcfa6c4bdd6351ab6f5f880
Brandon Nouhavandi <brandonnouhavandi@gmail.com>
2020-03-13 09:56:10 -0700

    custom scope

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,12 @@
 class Portfolio < ApplicationRecord
 	validates_presence_of :title, :body, :main_image, :thumb_image
+
+def self.angular
+	where(subtitle: 'Angualar')
+end 
+
+
+scope :ruby_on_rails_portfolio_items, -> {where(subtitle: 'Angualar')
+}
 end
  
