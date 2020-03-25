defeb9e8c50ec8c145803919de9d167240808413
Brandon Nouhavandi <brandonnouhavandi@gmail.com>
2020-03-25 10:23:41 -0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  get '/logout', to: 'devise/sessions#destroy'
 
   resources :blogs do
     member do
